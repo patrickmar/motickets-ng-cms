3031d041bdd3b9df8e9a212fb5beee4ef5c00931
sundayuchegbu <sundayuchegbu10@gmail.com>
2025-05-15 13:08:09 +0100

    first commit

---
+++ src/services/index/user.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export const signup = async ({ name, email, password }) => {
+  try {
+    const { data } = await axios.post(
+      "https://avante-css.onrender.com/api/v1/register",
+      {
+        name,
+        email,
+        password,
+      }
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const login = async ({ email, password }) => {
+  try {
+    const { data } = await axios.post(
+      "https://avante-css.onrender.com/api/v1/login",
+      {
+        email,
+        password,
+      }
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
